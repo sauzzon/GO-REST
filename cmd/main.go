c7737c544edbcad80100fc597b501fea6c9f052b
sauzzon <saujan.tiwari@gmail.com>
2022-11-15 08:56:37 +0545

    hot reload with air

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ func main() {
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!!")
+		return c.SendString("HelloWorld from Docker Container!!")
 	})
 
 	app.Listen(":3000")
