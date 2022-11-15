60897c2d64f0f326ecd9db489c3c55014af02619
sauzzon <saujan.tiwari@gmail.com>
2022-11-15 10:31:32 +0545

    creating database connection

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"www.github.com/sauzzon/GO-REST/database"
+)
 
 func main() {
+	database.ConnectDb()
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
